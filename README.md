c0d67f47f4c589b6d98f88b8191db17e3a7a83be
Pieter van Goor <pieter.vangoor@anu.edu.au>
2020-11-24 11:13:57 +1100

    Improve plotting functionality

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This package implements common Lie groups in python.
 The idea is to have Lie groups interact as you would expect: multiplication operators are defined for group products as well as default group actions on vectors.
 There are also some analysis and plotting tools.
+
+## Installation
+
+Run setup and install:
+
+`python3 setup.py sdist bdist_wheel && python3 -m pip install dist/pylie-0.1.0.tar.gz`
--- pylie/plotting.py
+++ pylie/plotting.py
@@ -1,5 +1,6 @@
 try:
     import matplotlib.pyplot as plt
+    from matplotlib.artist import Artist
     from mpl_toolkits.mplot3d import Axes3D
 except ImportError as error:
     print("The plotting functionality of pylie depends on matplotlib.")
@@ -8,46 +9,65 @@ except ImportError as error:
 from pylie import SO3, SE3, SIM3, LieGroup
 import numpy as np
 
+class frameArtist(Artist):
+    def __init__(self, ax : Axes3D, pose_data : SE3, style='-'):
+        if not isinstance(ax, Axes3D):
+            raise TypeError("The axes must be of 3D type.")
+        if not isinstance(pose_data, (SIM3, SE3, SO3)):
+            raise TypeError("The pose_data must be of one of the types: SIM3, SE3, SO3.")
+
+        self._pose_data = pose_data
+        Q, t = self._pose_to_Qt(pose_data)
+
+        colors = ['r','g','b']
+        colors = [c+style for c in colors]
+        self._frame_lines = []
+        for a in range(3):
+            temp, = ax.plot([t[0,0], t[0,0]+Q[0,a]], [t[1,0], t[1,0]+Q[1,a]], [t[2,0], t[2,0]+Q[2,a]], colors[a])
+            self._frame_lines.append(temp)
+        self._frame_center, = ax.plot(t[0,0], t[1,0], t[2,0], 'ko')
+    
+    def set_pose_data(self, pose : SE3):
+        if not isinstance(pose, (SIM3, SE3, SO3)):
+            raise TypeError("The frame must be of one of the types: SIM3, SE3, SO3.")
+        
+        self._pose_data = pose
+        Q, t = self._pose_to_Qt(pose)
+        for a in range(3):
+            self._frame_lines[a].set_data_3d([t[0,0], t[0,0]+Q[0,a]], [t[1,0], t[1,0]+Q[1,a]], [t[2,0], t[2,0]+Q[2,a]])
+        self._frame_center.set_data_3d(t[0,0], t[1,0], t[2,0])
+
+    
+    @staticmethod
+    def _pose_to_Qt(pose : SE3):
+        if isinstance(pose, SO3):
+            mat = np.eye(4)
+            mat[0:3,0:3] = pose.as_matrix()
+        else:
+            mat = pose.as_matrix()
+        
+        Q = mat[0:3,0:3]
+        t = mat[0:3,3:4]
+        return Q, t
+
+
 def plotFrame(frame : LieGroup, style='-', ax : Axes3D = None):
-    if not isinstance(frame, (SIM3, SE3, SO3)):
-        raise TypeError("The frame must be of one of the types: SIM3, SE3, SO3.")
     if ax is None:
         ax = plt.gca()
-    if not isinstance(ax, Axes3D):
-        raise TypeError("The axes must be of 3D type.")
-
-    if isinstance(frame, SO3):
-        mat = np.eye(4)
-        mat[0:3,0:3] = frame.as_matrix()
-    else:
-        mat = frame.as_matrix()
     
-    Q = mat[0:3,0:3]
-    t = mat[0:3,3:4]
+    frame_artist = frameArtist(ax, frame, style)
 
-    frame_axes = [[],[],[]]
-    for a in range(3):
-        for i in range(3):
-            frame_axes[a] += [t[a,0], t[a,0]+Q[a,i], np.nan]
-
-    # result = ax.plot(frame_axes[0], frame_axes[1], frame_axes[2])
-    colors = ['r','g','b']
-    colors = [c+style for c in colors]
-    result = []
-    for a in range(3):
-        temp = ax.plot([t[0,0], t[0,0]+Q[0,a]], [t[1,0], t[1,0]+Q[1,a]], [t[2,0], t[2,0]+Q[2,a]], colors[a])
-        result.append(temp)
-    temp = ax.plot(t[0,0], t[1,0], t[2,0], 'ko')
-    result.append(temp)
-
-    return result
+    return frame_artist
 
 if __name__ == "__main__":
     pose = SE3.identity()
     pose = SE3.from_list([1,2,3], "x")
+    pose2 = SE3.from_list([4,5,6], "x")
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    plotFrame(pose)
+    artist = plotFrame(pose)
+    plt.show()
 
+    artist.set_pose_data(pose2)
     plt.show()
