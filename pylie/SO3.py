a4d11b4735904f5c4a7bad8eb55b13e3b8e2047b
Pieter van Goor <pieter.vangoor@anu.edu.au>
2021-05-24 11:56:14 +1000

    Add SO3 from vectors

--- pylie/SO3.py
+++ pylie/SO3.py
@@ -176,6 +176,22 @@ class SO3(LieGroup):
     @staticmethod
     def skew(vec : np.ndarray) -> np.ndarray:
         return SO3.wedge(vec)
+    
+    @staticmethod
+    def from_vectors(origin : np.ndarray, dest : np.ndarray) -> 'SO3':
+        # origin = origin / np.linalg.norm(origin)
+        # dest = dest / np.linalg.norm(dest)
+        # v = np.cross(origin, dest, axis=0);
+        # c = origin.T @ dest
+
+        # vx = SO3.skew(v)
+        # mat = np.eye(3) + (vx + 1 / (1 + c) * vx @ vx);
+        # if (abs(1 + c) <= 1e-8):
+        #     raise ValueError("The vectors cannot be exactly opposing.")
+
+        # return SO3.from_matrix(mat)
+        result = SO3(Rotation.align_vectors(dest.T, origin.T))
+        return result
 
 if __name__ == "__main__":
     R = SO3()
